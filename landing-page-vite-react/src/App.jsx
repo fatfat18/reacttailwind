bcf799c21a38da67fd5ab3b2f70bb0a942e13cc1
PatrickPahunang <patrickcd452@gmail.com>
2023-02-19 14:25:12 +0800

    VITE+REACT+TAILWIND

---
+++ landing-page-vite-react/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import './index.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div className='flex justify-evenly test'>
+        <a href="#" target="_blank" className='contain'>
+          <img src="/vite.svg" className="logo h-32" alt="Vite logo" data-aos="fade-right" aos-duration="500" />
+        </a>
+        <a href="#" target="_blank">
+          <img src={reactLogo} className="logo react h-32" alt="React logo" data-aos="fade-left" aos-duration="500" />
+        </a>
+      </div>
+      <p className='text-5xl text-gray-900'>Vite + React</p>
+      <div className="card">
+        <button className='button1' onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
