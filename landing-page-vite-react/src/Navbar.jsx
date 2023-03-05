6498bf65e91cb095a044d89db6eafdb5fb869434
PatrickPahunang <patrickcd452@gmail.com>
2023-03-05 09:42:14 +0800

    sample

--- landing-page-vite-react/src/Navbar.jsx
+++ landing-page-vite-react/src/Navbar.jsx
@@ -10,23 +10,30 @@ export const Navbar = () => {
 
     const navLinkStyles = ({ isActive }) => {
         return {
-            fontWeight: isActive ? 'bold' : 'normal',
-            color: isActive? 'tan' : 'white',
+            fontWeight: isActive ? '900' : 'lighter',
+            color: isActive? 'red' : 'black',
+            borderTop: isActive? 'thick solid black': 'white',
+            borderBottom: isActive? 'thick solid black': 'white',
+    
+            
+          
+          
         }
     }
 
     return (
-        <nav className='text-2xl bg-neutral-900 xl:h-16 justify-around items-center flex w-screen '>
+        <nav className='text-2xl bg-white xl:h-16 justify-around items-center flex w-screen'>
             <ul>
-                <li className=''>
-                    <NavLink to="/nike" style={navLinkStyles} className="px-4"> Nike </NavLink>
-                    <NavLink to="/adidas"  style={navLinkStyles} className="px-4"> Adidas </NavLink>
-                    <NavLink to="/converse" style={navLinkStyles} className="px-4"> Converse </NavLink>
-                    <NavLink to="/puma"  style={navLinkStyles} className="px-4"> Puma </NavLink>
-                    <NavLink to="/vans" style={navLinkStyles} className="px-4"> Vans </NavLink>
-
+                <li className='transition ease-in-out delay-300'>
+                    <NavLink to="/nike" style={navLinkStyles} className="px-8 xl:py-4"> NIKE </NavLink>
+                    <NavLink to="/adidas"  style={navLinkStyles} className="px-8 xl:py-4"> ADIDAS </NavLink>
+                    <NavLink to="/converse" style={navLinkStyles} className="px-8 xl:py-4"> CONVERSE </NavLink>
+                    <NavLink to="/puma"  style={navLinkStyles} className="px-8 xl:py-4"> PUMA </NavLink>
+                    <NavLink to="/vans" style={navLinkStyles} className="px-8 xl:py-4"> VANS </NavLink>
                 </li>
             </ul>
+
+    
         </nav>
     )
 
--- landing-page-vite-react/src/shoecomponents/puma.jsx
+++ landing-page-vite-react/src/shoecomponents/puma.jsx
@@ -1,10 +1,37 @@
-import { Link } from 'react-router-dom';
-import { Navbar } from '../Navbar';
+import S1 from '../assets/S1.png';
+import S2 from '../assets/S2.png';
+import S3 from '../assets/S3.png';
+import S4 from '../assets/S4.png';
+import S5 from '../assets/S5.png';
+import S6 from '../assets/S6.png';
+import S7 from '../assets/S7.png';
+import S8 from '../assets/S8.png';
+import S9 from '../assets/S9.png';
+
+
+
+
 
 export default function Puma() {
+
+
+
+    
     return (
         <>
-            <h1 className='flex justify-center text-center items-center'> THIS IS THE PUMA PAGE </h1>
+
+        <div className='flex flex-wrap justify-center items-center mt-8 z-50 basis-1/ w-screen h-max overflow-y-hidden'>
+            <a href='#' className='hover:scale-110 transition ease-in 2s'><img src={S1}   data-aos-once="false" data-aos="fade-up" data-aos-duration="900" className=' rounded-xl mx-4 my-4 ' /></a>
+            <a href='#' className='hover:scale-110 transition ease-in 2s'><img src={S2}   data-aos-once="false"  data-aos="fade-up" data-aos-duration="1000"  className=' rounded-xl mx-4 my-4 ' /></a>
+            <a href='#' className='hover:scale-110 transition ease-in 2s'><img src={S3}   data-aos-once="false" data-aos="fade-up" data-aos-duration="1100"  className=' rounded-xl mx-4 my-4 ' /></a>
+            <a href='#' className='hover:scale-110 transition ease-in 2s'><img src={S4}   data-aos-once="false" data-aos="fade-up" data-aos-duration="1200"  className=' rounded-xl mx-4 my-4 ' /></a>
+            <a href='#' className='hover:scale-110 transition ease-in 2s'><img src={S5}   data-aos-once="false" data-aos="fade-up" data-aos-duration="1300"  className=' rounded-xl mx-4 my-4 ' /></a>
+            <a href='#' className='hover:scale-110 transition ease-in 2s'><img src={S6}   data-aos-once="false" data-aos="fade-up" data-aos-duration="1400"  className=' rounded-xl mx-4 my-4 ' /></a>
+            <a href='#' className='hover:scale-110 transition ease-in 2s'><img src={S7}   data-aos-once="false" data-aos="fade-up" data-aos-duration="1500"  className=' rounded-xl mx-4 my-4 ' /></a>
+            <a href='#' className='hover:scale-110 transition ease-in 2s'><img src={S8}   data-aos-once="false" data-aos="fade-up" data-aos-duration="1600"  className=' rounded-xl mx-4 my-4 ' /></a>
+            <a href='#' className='hover:scale-110 transition ease-in 2s'><img src={S9}   data-aos-once="false" data-aos="fade-up" data-aos-duration="1700"  className=' rounded-xl mx-4 my-4 ' /></a>
+        </div>
+            
         </>
     )
 }
